6d0ce667c46762f06d96cbf2c15a033aa8da1893
Mateo Blandon Mesa <matius-b@hotmail.com>
2023-04-17 10:19:08 -0500

    Created the Windows Form Project in Visual Studio, Add Person Class.

---
+++ SQLDataAccessDemo/FormUI/Dashboard.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace FormUI
+{
+  partial class Dashboard
+  {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // Dashboard
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1200, 692);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+      this.Name = "Dashboard";
+      this.Text = "SQL Data Access Demo";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
